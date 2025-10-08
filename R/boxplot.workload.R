c171d6eda5fb3463ccff040060e0e185595b9ceb
Simon <simon_eins@gmx.de>
2025-10-08 19:51:44 +0200

    Sinnvolle Beispieldaten bei für ´boxplot.workload()´ ergänzt. Die bisher vorhandenene falsche Verschiebung der Box um einen Einheit nach rechts (sodass z. B. bei Veranstaltungen mit 0h stattdessen 1h angezeigt wird) entfernt. Code vereinfacht. Keine 2 Plots übereinander mehr.

--- R/boxplot.workload.R
+++ R/boxplot.workload.R
@@ -1,26 +1,24 @@
 #' Boxplot mit Workloads der LVs: Funktioniert, sollte überarbeitet werden
 #' 
 #' @param x Daten
-#' @param q Skala x Achse
-#' @param e Anzahl der Antwortoptionen der Skala
-#' @param n Anzahl der Veranstaltungen
+#' @param skala Skala x-Achse
 #'
-#' @examples boxplot.workload(x = BspDaten$dataLVE$WL)
+#' @examples boxplot.workload(x = BspDaten$Plots$WL)
 #' 
 #' @returns Boxplot
 #' 
 #' @export boxplot.workload
 
 boxplot.workload <- function(x, # Daten
-                             q = c("0h", "1h", "2h", "3h", "4h", "5h", "6h", "7h",
-                                   "8h", "9h", "10h", "11h", "12h", "mehr als\n12h"), # Skala x Achse
-                             e = 13, # Anzahl der Antwortoptionen der Skala
-                             n = NA) # Anzahl der Veranstaltungen
+                             skala = c("0h", "1h", "2h", "3h", "4h", "5h", "6h", "7h",
+                                   "8h", "9h", "10h", "11h", "12h", "mehr als\n12h")) # Skala x-Achse
+
 {
   
-  # Berechnung der Anzahl der Veranstaltungen falls nicht angegeben -------
+  # Berechnung der Anzahl der Veranstaltungen und Länge der Skala -------
   
-  if(is.na(n)) {n <- length(x)}
+  n <- length(x)
+  n_skala <- length(skala)
   
   # Bisherige Grafikparameter speichern -----------------------------------
   
@@ -30,28 +28,38 @@ boxplot.workload <- function(x, # Daten
   
   par(fg = "gray80",  # Farbe des Grid (die Hilfslinen) festlegen
       family = set.analysis.defaults$font.family, # Schriftart festlegen
-      mar=c(7, 4.1, 2, 4.2)) # Seitenränder festlegen
+      mar=c(7, 4.1, 2, 4.2), # Seitenränder festlegen
+      lend = "square", # Linienenden eckig
+      ljoin = "mitre") # Linienschnitt eckig
   
-  # Plot erstellen (nur damit gleich die Vertikalen Linien stimmen) -------
+  # Leeren Plot zeichnen (um Hilfslinien drüber zu legen) -----------------
   
-  boxplot(x, ylab = NULL, xlab = NULL, horizontal = TRUE, ylim = c(0,e),
-          xaxt = "n", yaxt = "n", boxwex = 0.8)
+  plot(
+    1,  # Dummy-Wert
+    type = "n",  # Keine Daten zeichnen
+    xlim = c(1, n_skala),  # x-Achse: Skalenwerte (horizontaler Boxplot!)
+    xlab = "", ylab = "",  # Keine Achsenbeschriftung
+    xaxt = "n", yaxt = "n",  # Keine Achsen zeichnen
+    bty = "n"  # Kein Rahmen
+  )
   
-  # Einfügen von vertikalen Linien
   
-  abline(v = c(0:e))
+  # Einfügen von vertikalen Linien ----------------------------------------
+  
+  abline(v = c(1:n_skala))
   
   # Plot über die Vertikalen Linien drüber plotten ------------------------
 
-  boxplot(x, ylab = NULL, xlab = NULL, horizontal = TRUE, ylim=c(0,e),
-          xaxt = "n", yaxt = "n", boxwex = 0.8, border="black",
-          pars=list(outcol = set.analysis.defaults$color.bars, outpch = 20),
+  boxplot(x, ylab = NULL, xlab = NULL, horizontal = TRUE, ylim=c(1:n_skala),
+          xaxt = "n", yaxt = "n", #boxwex = 0.8,
+          border="black",
+          pars =list(outcol = set.analysis.defaults$color.bars, outpch = 20),
           col = set.analysis.defaults$color.bars, add = TRUE)
   
   # Achsenmarkierungen und Beschriftungen einfügen ------------------------
   
-  axis(side = 1, at = 0:e, labels = FALSE)
-  mtext(q,side=1, line=1, at=0:e, las=1, col="black", cex=0.8)
+  #axis(side = 1, at = 1:n_skala, labels = FALSE)
+  mtext(skala, side = 1, line = 1, at = 1:n_skala, las=1, col="black", cex=0.8)
   
   info1 <- "angegebener Workload der LV"
   info2 <- paste0("[n = ", n, "]")
