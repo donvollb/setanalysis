eff8121222894219b23ea73a154cba049134e43a
Simon <simon_eins@gmx.de>
2025-11-10 17:00:00 +0100

    `boxplot.aggr.sk()` mit Hilfsfunktionen vereinfacht. Möglichkeit Schriftart und Farbe direkt in der Funktion zu ändern entfernt (das sollte über die defaults geschehen).

--- R/boxplot.aggr.sk.R
+++ R/boxplot.aggr.sk.R
@@ -3,8 +3,6 @@
 #' @param x Daten
 #' @param item_labels Labels/Text/Beschriftungen der Y-Achse
 #' @param skala Skala der x-Achse
-#' @param color Farbe der Boxen
-#' @param font_family Schriftart
 #'
 #' @returns Boxplot
 #' 
@@ -16,73 +14,53 @@
 #' 
 #' @export boxplot.aggr.sk
 
-boxplot.aggr.sk <- function(x, # Daten
+boxplot.aggr.sk <- function(x,           # Daten
                             item_labels, # Labels/Text/Beschriftungen der Y-Achse
-                            skala, # Skala der x-Achse
-                            color = setanalysis_defaults$color.bars,
-                            font_family = setanalysis_defaults$font.family)
+                            skala)       # Skala der x-Achse
 {
   daten <- cbind(x)
   n_items <- ncol(daten)
   n_skala <- length(skala)
   
+  # Bisherige Grafikparameter speichern -----------------------------------
+  
   opar <- par(no.readonly = TRUE)
+  
+  # Grafikparameter für den Plot einstellen -------------------------------
+  
+  .common_par()
   
   if(n_skala == 5) {
     par(mar = c(4, 21.5, 0.1, 2.1)) # mehr Platz für Hinweistext bei 5er Skala
     } else {
     par(mar = c(2.1, 21.5, 0.1, 2.1))
   }
- 
 
-  par(family = font_family,
-          fg = "gray50",
-        lend = "square", # Linienenden eckig
-       ljoin = "mitre")  # Linienschnitt eckig
-  
   # Leeren Plot zeichnen (um Hilfslinien drüber zu legen) -----------------
   
-  plot(
-    1,  # Dummy-Wert
-    type = "n",  # Keine Daten zeichnen
-    xlim = c(1, n_skala),  # x-Achse: Skalenwerte
-    ylim = c(0.5, length(daten) + 0.5),  # y-Achse: Anzahl der Gruppen + Puffer
-    xlab = "", ylab = "",  # Keine Achsenbeschriftung
-    xaxt = "n", yaxt = "n",  # Keine Achsen zeichnen
-    bty = "n"  # Kein Rahmen
-  )
+  .empty_plot(xlim = c(1, n_skala),
+              ylim = c(0.5, n_items + 0.5))
   
+  # Hilfslinien -----------------------------------------------------------
   
-  abline(v = c(1:n_skala), col="gray80") #vertikale Hilfslinien bei jedem Skalenpunkt
+  abline(v = c(1:n_skala), col = "grey70")
   
-  boxplot(daten,
-          add = TRUE,
-          xaxt = "n",
-          yaxt = "n",
-          xlim = c(1, n_skala), horizontal = TRUE,
-        boxwex = 0.8, col = color, border = "black",
-          pars = list(outcol = color, outpch = 20))
+  # Eigentlichen Boxplot zeichnen -----------------------------------------
   
-  mtext(item_labels,
-        side = 2,
-        line = 1,
-          at = 1:n_items,
-         las = 2,
-         col = "gray30", # Farbe der Beschriftung
-         cex = 1)        # Schriftgröße
+  .costum_boxplot(x, boxwex = 0.8,
+                  ylim = c(0.5, n_items + 0.5),
+                  xlim = c(1, n_skala))
   
-  mtext(skala,
-        side = 1,
-        line = 1,
-          at = 1:n_skala,
-         las = 1,
-         col = "gray30",
-         cex = 1,
-        font = 2)
+  # Achsenbeschriftungen hinzufügen --------------------------------------
+  
+  .text_left(item_labels, at = 1:n_items)
+  
+  .text_bottom(skala, at = 1:n_skala)
   
   if (n_skala == 5) {
     mtext("Hinweis: andere Skalenlogik (im Vergleich zu den 6er-Skalen)",
-          side = 1, line = 3, col = "gray40", font = 3)}
+          side = 1, line = 3, col = "gray15", font = 3)}
+  # Vorher gesicherte Grafikparameter wiederherstellen --------------------
   
   par(opar)
 }
