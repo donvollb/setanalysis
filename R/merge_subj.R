81571e5b0580ce7f3d1d99505944e3bbf46e1144
Simon <simon_eins@gmx.de>
2026-02-26 19:22:27 +0100

    Umbenennung von `merge.subj()` zu `merge_subj()`, Kommentare zum Code hinzugefüht

--- R/merge_subj.R
+++ R/merge_subj.R
@@ -9,18 +9,22 @@
 #'
 #' @examples
 #' 
-#' merge.subj(BspDaten$dataSHOWUP$fach1_2FB,
+#' merge_subj(BspDaten$dataSHOWUP$fach1_2FB,
 #'            BspDaten$dataSHOWUP$fach2_2FB) |> markdown_in_viewer()
 #' 
-#' @export merge.subj
+#' @export merge_subj
 
-merge.subj <- function(x1, # Daten von Fach 1
+merge_subj <- function(x1, # Daten von Fach 1
                        x2, # Daten von Fach 2
                        inkl1 = "nr1", # TRUE oder FALSE, ob die Funktion ausgeführt wird; "nr1" zieht sich automatisch die entsprechende inkl. Variable
                        inkl2 = "nr2", # TRUE oder FALSE, ob die Funktion ausgeführt wird; "nr2" zieht sich automatisch die entsprechende inkl. Variable
                        nr1 = "", # Nummer, die Grundlage für entsprechende inkl. Variable ist und vorne an den Fragetext gestellt wird
                        nr2 = "") # Nummer, die Grundlage für entsprechende inkl. Variable ist und vorne an den Fragetext gestellt wird
 {
+
+
+  # Überprüfung der inkl-Parameter ----------------------------------------
+
   if (inkl1 == "nr1") {
     if (nr1 == "") {inkl1 <- TRUE} else {inkl1 <- eval(parse(text = paste0("inkl.", nr1)))}
   }
@@ -29,21 +33,32 @@ merge.subj <- function(x1, # Daten von Fach 1
     if (nr2 == "") {inkl2 <- TRUE} else {inkl2 <- eval(parse(text = paste0("inkl.", nr2)))}
   }
 
-  if (inkl1 == TRUE && inkl2 == TRUE) {
+  if (inkl1 != TRUE | inkl2 != TRUE) {
+    return(invisible())} # wenn nicht beide inkl TRUE sind, wird Funktion beendet
 
-    #    subj1 <- data.frame(fach = unlist(x1, use.names = FALSE))
-    #    subj2 <- data.frame(fach = unlist(x2, use.names = FALSE))
+  
+  # Zusammenfügen der beiden Fächer-Spalten -------------------------------
+  
+  subj <- rbind(data.frame(fach = unlist(x1, use.names = FALSE)),
+                  data.frame(fach = unlist(x2, use.names = FALSE))) 
 
-    subj <- rbind(data.frame(fach = unlist(x1, use.names = FALSE)),
-                  data.frame(fach = unlist(x2, use.names = FALSE))) # Zusammenfügen der beiden Fächer-Spalten
+  # Vergabe des neuen Labels ----------------------------------------------
+  
+  attr(subj$fach, "label") <- paste0(" & ", nr2, " ",sub("\\?.*", "",
+                                     attr(subj$fach, "label")), " / 2. Fach? ")
+  
+  # Aufruf der merge_sc-Funktion ------------------------------------------
+  
+  merge_sc(subj$fach, nr = nr1)
 
-    attr(subj$fach, "label") <- paste0(" & ", nr2, " ",
-                                       sub("\\?.*", "", attr(subj$fach, "label")),
-                                       " / 2. Fach? ")  # Vergabe des neuen Labels
-
-    merge.sc(subj$fach, nr = nr1) # Aufruf der merge.sc-Funktion
-
-    cat("*Hinweis: In der Befragung wurden 1. und 2. Fach getrennt abgefragt; in dieser Tabelle werden die Antworten gemeinsam dargestellt. Daraus ergibt sich in dieser Darstellung eine Verdopplung des Stichprobenumfangs (siehe „Total“).*  \n  \n")
-
-  }
+  cat(paste("*Hinweis: In der Befragung wurden 1. und 2. Fach getrennt abgefragt;",
+            "in dieser Tabelle werden die Antworten gemeinsam dargestellt.",
+            "Daraus ergibt sich in dieser Darstellung eine Verdopplung des",
+            "Stichprobenumfangs (siehe „Total“).*  \n  \n"))
 }
+
+
+#' @noRd
+#' @export merge.subj
+
+merge.subj <- merge_subj
