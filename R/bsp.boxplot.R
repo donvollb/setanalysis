39f09813223afd9c565ae9b9c8828fe5e4aaf0ff
Simon <simon_eins@gmx.de>
2026-04-01 17:17:25 +0200

    `bsp.boxplot()`: massive optische Verschönerung

--- R/bsp.boxplot.R
+++ R/bsp.boxplot.R
@@ -19,9 +19,16 @@ bsp.boxplot <- function(x = "default") # Daten, bei "default" wird ein Beispield
   
 
   subchunkify(c(
+    
+    
+    
+
     opar <- par(no.readonly = TRUE),
     
-    .common_par(mar = c(4.8, 11, 4.1, 6)),
+
+    # Grafikparameter für den Plot einstellen -----------------------------
+
+    .common_par(mar = c(7, 8, 5, 2.1)),
     
     # Leeren Plot zeichnen (um Hilfslinien drüber zu legen) ---------------
     
@@ -33,7 +40,7 @@ bsp.boxplot <- function(x = "default") # Daten, bei "default" wird ein Beispield
     
     # Eigentlicher Plot ---------------------------------------------------
     
-    .costum_boxplot(x, boxwex = 0.5, ylim = c(1, 6)),
+    .costum_boxplot(x, boxwex = 0.8, ylim = c(1, 6)),
 
     # Achsenbeschriftungen hinzufügen ------------------------------------
     
@@ -44,22 +51,26 @@ bsp.boxplot <- function(x = "default") # Daten, bei "default" wird ein Beispield
     
     # Beschriftungslinien -------------------------------------------------
     
-    segments(x0 = 1.7, y0 = 1.08, x1 = 1.7, y1 = 1.76, col = "grey15"),
-    segments(x0 = 4.5, y0 = 1.26, x1 = 4.5, y1 = 1.76, col = "grey15"),
-    segments(x0 = 5.7, y0 = 1.17, x1 = 5.7, y1 = 1.76, col = "grey15"),
-    segments(x0 = 1.1, y0 = 0.12, x1 = 2.8, y1 =-0.20, col = "grey15"),
-    segments(x0 = 5.9, y0 = 0.12, x1 = 4.2, y1 =-0.20, col = "grey15"),
+    segments(x0 = 1.7, y0 = 1.08, x1 = 1.7, y1 = 1.66, col = "grey15"),
+    segments(x0 = 4.5, y0 = 1.26, x1 = 4.5, y1 = 1.66, col = "grey15"),
+    segments(x0 = 5.7, y0 = 1.17, x1 = 5.7, y1 = 1.66, col = "grey15"),
+    segments(x0 = 1.1, y0 = 0.25, x1 = 2.8, y1 = 0.00, col = "grey15"),
+    segments(x0 = 5.9, y0 = 0.25, x1 = 4.2, y1 = 0.00, col = "grey15"),
     
     # Beschriftungstexte --------------------------------------------------
     
-    text(x = 1.7, y = 1.9, col = "grey15", label = "Ausreißer"),
-    text(x = 4.5, y = 1.9, col = "grey15", label = "Median"),
-    text(x = 5.7, y = 1.9, col = "grey15", label = "Max"),
-    text(x = 3.5, y =-0.2, col = "grey15", label = "6-stufige Skala")),
-    
-    fig_height = 2.7, # optimal: 2.7
-    fig_width = 10,   # optimal: 10
-    hide = TRUE)      # damit nicht Text dazu "ausgespuckt" wird
+    text(x = 1.7, y = 1.8, col = "grey15", label = "Ausreißer"),
+    text(x = 4.5, y = 1.8, col = "grey15", label = "Median"),
+    text(x = 5.7, y = 1.8, col = "grey15", label = "Max"),
+    text(x = 3.5, y = 0.0, col = "grey15", label = "6-stufige Skala"),
+
+    # Vorher gesicherte Grafikparameter wiederherstellen ------------------
+
+    par(opar)),
+   
+    fig_height = 3.8,
+    fig_width  = 9,
+    hide = TRUE)
 
   cat("  \n  \n")
 
